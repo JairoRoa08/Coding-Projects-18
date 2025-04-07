ad98c559ca27c49b51377579e35aacd1f9f7ebb3
Jairo Roa <Jroa@usf.edu>
2025-04-06 21:50:35 -0400

    Task 1: Create a New Component

---
+++ my-react-apps/src/components/Greetings.jsx
@@ -0,0 +1,13 @@
+// Import
+import React from 'react';
+// Greeting Function
+function Greeting () {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is your first custom component in React.</p>
+    </div>
+  );
+};
+// Export
+export default Greeting;
